d87f8a911f116647da796a85c89d13719bf1946e
Arjoonn Sharma <arjoonn.94@gmail.com>
2018-04-18 16:14:29 +0530

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+Chiller
+=======
+
+
+VERY minimal javascript constructs I keep around for use in personal projects when I don't feel like importing a full framework
---
+++ chiller.js
@@ -0,0 +1,17 @@
+byId = document.getElementById;
+select = document.querySelector;
+
+
+function post(url, data, success){
+    var data = JSON.stringify(data);
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            var dat = JSON.parse(xmlhttp.responseText);
+            success(dat);
+         }
+     }
+    xmlhttp.send(data);
+}
